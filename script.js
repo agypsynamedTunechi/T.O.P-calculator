5077914a8fcedce15a5ca41eaf75f2b0fae368d8
aypsynamedTunechi <agypsynamedtunechi@gmail.com>
2025-08-02 16:10:26 +0100

    Update button IDs and styles: rename number button to decimal, adjust container dimensions

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const numberDisplay = document.querySelector("#number-display");
 // const totalDisplay = document.querySelector("#total");
 const numbers = document.querySelectorAll("#number");
 const operators = document.querySelectorAll("#operator");
+const decimal = document.querySelector("#decimal");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
 
 function add(a, b) {
   let total = Number(a) + Number(b);
@@ -31,6 +34,7 @@ function modulus(a, b) {
 let firstNum = "";
 let secondNum = "";
 let operator = "";
+let decimalIsAllowed = true;
 
 function operate(num1, op, num2) {
   switch (op) {
@@ -57,6 +61,7 @@ function operate(num1, op, num2) {
 }
 
 function display() {
+  
   numbers.forEach((number) => {
     number.addEventListener("click", () => {
       let numberValue = number.getAttribute("value");
@@ -65,36 +70,84 @@ function display() {
         numberDisplay.textContent = firstNum;
       } else {
         secondNum += numberValue;
-        numberDisplay.textContent += secondNum;
+        numberDisplay.textContent = secondNum;
       }
     });
   });
 
+  decimal.addEventListener("click", () => {
+    let decimalValue = decimal.getAttribute("value");
+    if (decimalIsAllowed) {
+      if(firstNum){
+        firstNum += decimalValue;
+        numberDisplay.textContent = firstNum;
+      }else{
+        secondNum += decimalValue;
+        numberDisplay.textContent = secondNum;
+      }
+
+      
+      decimalIsAllowed = false;
+      return;
+    }
+  });
+
   operators.forEach((op) => {
     op.addEventListener("click", () => {
+      decimalIsAllowed = true;
       let operatorValue = op.getAttribute("value");
       if (operatorValue !== "=") {
-        if(operator !== ""){
-            operate(firstNum, operator, secondNum);
-            firstNum = numberDisplay.textContent;
-            secondNum = "";
-            operator = operatorValue;
-            numberDisplay.textContent += operator;
-        }else{
-            operator = operatorValue;
-            numberDisplay.textContent += operator;
+        if (operator !== "") {
+          operate(firstNum, operator, secondNum);
+          firstNum = numberDisplay.textContent;
+          secondNum = "";
+          operator = operatorValue;
+          numberDisplay.textContent += operator;
+        } else {
+          operator = operatorValue;
+          numberDisplay.textContent += operator;
         }
-     }else {
+      } else {
         operate(firstNum, operator, secondNum);
-        if(!operator){
-          firstNum = 0
-        }else{
+        console.log(operator);
         firstNum = numberDisplay.textContent;
-            secondNum = "";
-        }
+        operator = "";
       }
+      secondNum = "";
+      console.log(`first num = ${firstNum}`);
+      console.log(operator);
+      console.log(`second num = ${secondNum}`);
     });
   });
+
+  clear.addEventListener("click", () => {
+    if (numberDisplay.textContent !== "") {
+      firstNum = "";
+      secondNum = "";
+      operator = ""
+      numberDisplay.textContent = "";
+    }else{
+      return
+    }
+  });
+
+  del.addEventListener("click", () => {
+    let str = numberDisplay.textContent
+    if(str !== "" && str.length > 0){
+      if(secondNum !== ""){
+      secondNum = secondNum.slice(0, -1);
+      console.log(`del 2nd num : ${secondNum}`)
+     }else if(operator !== "" ){
+      operator = operator.slice(0, -1);
+      console.log(`del operator : ${operator}`)
+     } else if(secondNum === "" && operator === ""){
+      firstNum = firstNum.slice(0, -1);
+      console.log(`del first num : ${firstNum}`)
+     }
+    numberDisplay.textContent = str.slice(0, -1)
+    }
+    
+  })
 }
 
 display();
